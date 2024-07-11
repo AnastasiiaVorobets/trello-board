982c983aa2d21aeffd6b239c6dca60917927547e
AnastasiiaVorobets <anastasiiavorobets03@gmal.com>
2024-07-11 17:40:34 +0300

    client: added message when board is empty

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -34,23 +34,29 @@ const Board = () => {
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <div className="board-container">
-      <CreateList />
+        <CreateList />
         <div className="lists-container">
-          {lists.map(list => (
-            <Droppable key={list._id} droppableId={list._id}>
-              {(provided) => (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                  <List
-                    key={list._id}
-                    list={list}
-                    provided={provided}
-                    cards={cards.filter(card => card.listId && card.listId._id === list._id)}
-                  />
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          ))}
+          {lists.length === 0 ? (
+            <div className="empty-board-message">
+              The board is empty. Add a list to get started.
+            </div>
+          ) : (
+            lists.map(list => (
+              <Droppable key={list._id} droppableId={list._id}>
+                {(provided) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    <List
+                      key={list._id}
+                      list={list}
+                      provided={provided}
+                      cards={cards.filter(card => card.listId && card.listId._id === list._id)}
+                    />
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            ))
+          )}
         </div>
       </div>
     </DragDropContext>
